945c97cfe7d1015e631420211746ba74178ee7b5
kashish6398 <kashishgupta99017@gmail.com>
2026-05-27 21:46:57 +0530

    Add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Agence from './pages/Agence'
 
 const App = () => {
   return (
-    <div>
+    <div className='text-white'>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/agence' element={<Agence />} />
       </Routes>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Video from '../components/home/Video'
+import HomeHeroText from '../components/home/HomeHeroText.jsx'
+import HomeBottomText from '../components/home/HomeBottomText.jsx'
 
 const Home = () => {
+
   return (
-    <div>Home</div>
+    <div className='text-white'>
+      <div className='h-screen w-screen fixed'>
+        <Video />
+      </div>
+      <div className='h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between'>
+        <HomeHeroText />
+        <HomeBottomText />
+      </div>
+    </div>
   )
 }
 
