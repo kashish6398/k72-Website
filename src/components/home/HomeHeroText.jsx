6f1116a0297494111cca6277781b1bed1111b8d8
kashish6398 <kashishgupta99017@gmail.com>
2026-05-31 21:56:23 +0530

    Fix video path

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -9,7 +9,7 @@ const HomeHeroText = () => {
             <div className='lg:text-[7.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[6.5vw] leading-[10vw]'>
                 qui
                 <div className='lg:h-[5.5vw] lg:w-[13vw] h-[7vw] w-[16vw] rounded-full -mt-2 lg:-mt-1.5 overflow-hidden '>
-                    <video className='h-full w-full object-cover rounded-full overflow-hidden' autoPlay loop muted src="../../../public/video.mp4"></video>
+                    <video className='h-full w-full object-cover rounded-full overflow-hidden' autoPlay loop muted src="/video.mp4"></video>
                 </div>
                 génère
             </div>
