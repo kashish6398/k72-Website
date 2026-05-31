c67b542795feafc818c4a7193834c44f4ed88646
kashish6398 <kashishgupta99017@gmail.com>
2026-05-31 17:57:45 +0530

    correct some issues in wensite

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -5,13 +5,13 @@ const HomeBottomText = () => {
 
   return (
     <div className='font-[font2] flex items-center justify-center gap-2 '>
-      <p className='absolute lg:w-[17vw] w-64 lg:right-20 right-0 bottom-28  lg:bottom-72 font-[font1] lg:text-lg text-xs lg:leading-relaxed leading-tight'>
+      <p className='absolute lg:w-[17vw] w-64 lg:right-20 right-0 bottom-28  lg:bottom-72 font-[font1] lg:text-sm text-xs lg:leading-relaxed leading-tight'>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; K72 est une agence qui pense chaque action pour nourrir la marque. Demain, dans 5 mois et dans 5 ans. On cherche la friction qui crée l’étincelle pour générer de l’émotion. Pour assurer une relation honnête, on est sans filtre, on dit ce qui doit être dit, on fait ce qui doit être fait.</p>
-      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-44 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
-        <Link className='text-[6vw] lg:mt-6' to='/projects'>Projects</Link>
+      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-32 flex items-center px-3 pt-1 lg:px-12 border-white rounded-full uppercase'>
+        <Link className='text-[6vw] lg:text-[3.5vw] lg:mt-4' to='/projects'>Projects</Link>
       </div>
-      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50]  lg:h-44 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
-        <Link className='text-[6vw] lg:mt-6' to='/agence'>agence</Link>
+      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-32 flex items-center px-3 pt-1 lg:px-12 border-white rounded-full uppercase'>
+        <Link className='text-[6vw] lg:text-[3.5vw] lg:mt-4' to='/agence'>agence</Link>
       </div>
     </div>
   )
--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import Video from './video'
 
 const HomeHeroText = () => {
     return (
         <div className='font-[font1]  mt-72 lg:mt-0 pt-5 text-center'>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+            <div className='lg:text-[7.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[6.5vw] leading-[10vw]'>
                 L'étincelle
             </div>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[10vw]'>
+            <div className='lg:text-[7.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[6.5vw] leading-[10vw]'>
                 qui
-                <div className='h-[7vw] w-[16vw] rounded-full -mt-3 overflow-hidden '>
-                    <Video />
+                <div className='lg:h-[5.5vw] lg:w-[13vw] h-[7vw] w-[16vw] rounded-full -mt-2 lg:-mt-1.5 overflow-hidden '>
+                    <video className='h-full w-full object-cover rounded-full overflow-hidden' autoPlay loop muted src="../../../public/video.mp4"></video>
                 </div>
                 génère
             </div>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+            <div className='lg:text-[7.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[6.5vw] leading-[10vw]'>
                 la créativité
             </div>
         </div>
